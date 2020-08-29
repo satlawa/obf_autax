04db581d724cb009508c2767e024f878eb20e228
satlawa <p.satlawa@gmx.net>
2020-08-29 22:14:25 +0200

    added calculation of  oh

---
+++ ekl/GISOh.py
@@ -0,0 +1,73 @@
+'''
+******************************************************************************************
+Cut raster with shape
+
+input --> shape (.shp) polygons of forest stands (Waldorte))
+input --> raster (.tiff) raster file with hights of trees
+output --> tree hights (.csv) table with top hights (Oberhoehen) for all forest stands
+******************************************************************************************
+'''
+
+import os
+import numpy as np
+import pandas as pd
+import fiona
+import rasterio
+from rasterio.mask import mask
+
+class GISOh(object):
+
+    def __init__(self, shp_path, raster_path):
+
+        self.shp_path = shp_path
+        self.raster_path = raster_path
+
+    def main(self, output_path = os.path.join(os.getcwd(), "oh.csv")):
+
+        # load shapefile with polygons
+        with fiona.open(self.shp_path) as shapefile:
+            polygons = [feature for feature in shapefile]
+
+        # load raster with oh information
+        raster = rasterio.open(self.raster_path)
+
+        # compute oh
+        hight = []
+        for polygon in polygons:
+            # compute oh for every polygon
+            hight.append(self.func_compute_oh(polygon, raster))
+
+        # convert to numpy array
+        hight_np = np.asarray(hight)
+        # convert to pandas DataFrame
+        hight_df = pd.DataFrame(hight, columns=['wo', 'oh', 'area'])
+        # write to csv
+        hight_df.to_csv(output_path, index=False)
+
+    # compute oh
+    def func_compute_oh(self, geom_all, raster):
+
+        wo = str(geom_all['properties']['REVIER_NR']) + str(geom_all['properties']['ABTEILUNG']) + geom_all['properties']['UNTERABTEI']
+        area = geom_all['properties']['GEOM_Area']
+        geom = geom_all['geometry']
+
+        # cutting the raster
+        out_image, out_transform = mask(raster, [geom], crop=True)
+        #out_meta = raster.meta.copy()
+
+        # set -3.402823e+38 to nan
+        out_image[out_image<-3.3e+38]=np.nan
+
+        # flatten array
+        flat = out_image.flatten()
+
+        # delete nan
+        flat = flat[~np.isnan(flat)]
+
+        if flat.size > 0:
+            # calculate oh
+            oh = np.percentile(flat, 90)
+        else:
+            oh = 0
+
+        return wo, oh, area
